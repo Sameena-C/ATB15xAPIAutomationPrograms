71857283a4779cbfd23976f9d31bd99b702f73de
Sameena-C <sameena.c@gmail.com>
2026-04-15 00:56:02 +0530

    13Apr2026

---
+++ src/test/java/systemscenarios/ex_03_RestAssured_TestNG_Advance/GET/APITesting_Lab07_TestCase.java
@@ -0,0 +1,20 @@
+package systemscenarios.ex_03_RestAssured_TestNG_Advance.GET;
+
+import org.testng.annotations.Test;
+
+public class APITesting_Lab07_TestCase {
+    @Test
+    public void test_01(){
+
+    }
+
+    @Test(enabled = false)
+    public void test_02(){
+
+    }
+
+    @Test
+    public void test_03(){
+
+    }
+}
